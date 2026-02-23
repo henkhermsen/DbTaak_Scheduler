01fa753ce4afe7dddda13d2cbb8dfb8bad383858
henkhermsen <45244755+henkhermsen@users.noreply.github.com>
2026-02-23 17:14:54 +0100

    Update install-service.cmd

--- scripts/install-service.cmd
+++ scripts/install-service.cmd
@@ -1,13 +1,13 @@
-REM Kopieer publish output + rgwschedule.config naar bijvoorbeeld:
-REM C:\Apps\RGW.Scheduler\
+REM Kopieer publish output + Db_TaakScheduler.config naar bijvoorbeeld:
+REM C:\Apps\Db_TaakScheduler\
 
 REM Install:
-sc create RGW_Scheduler binPath= "D:\RGW\Scheduler\RGW_Scheduler.exe" start= auto
-sc start RGW_Scheduler
+sc create Db_TaakScheduler_Scheduler binPath= "D:\Db_TaakScheduler\Db_TaakScheduler.exe" start= auto
+sc start Db_TaakScheduler
 
 REM Stop/verwijder:
-REM sc stop RGW_Scheduler
-REM sc delete RGW_Scheduler
+REM sc stop Db_TaakScheduler
+REM sc delete Db_TaakScheduler
 
 REM Publish (voorbeeld):
 REM dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true /p:SelfContained=true
