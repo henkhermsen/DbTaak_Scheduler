639f02293fab3c227fa58857c99d1016176d64ad
henkhermsen <45244755+henkhermsen@users.noreply.github.com>
2026-02-23 17:11:22 +0100

    Update compile2.cmd

--- compile2.cmd
+++ compile2.cmd
@@ -1,5 +1,3 @@
-cd D:\RGW\scheduler\RGW.Scheduler\RGW.Scheduler
-
 "D:\Program Files\Microsoft Visual Studio\18\Community\dotnet\net8.0\runtime\dotnet.exe" add package System.Threading.AccessControl
 "D:\Program Files\Microsoft Visual Studio\18\Community\dotnet\net8.0\runtime\dotnet.exe" restore
 "D:\Program Files\Microsoft Visual Studio\18\Community\dotnet\net8.0\runtime\dotnet.exe" build
